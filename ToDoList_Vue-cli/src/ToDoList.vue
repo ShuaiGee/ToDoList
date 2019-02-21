36105257075700f51f81b9aa2f52c12619274f54
ShuaiGee <862376350@qq.com>
2019-02-21 23:01:46 +0800

    todolist

---
+++ ToDoList_Vue-cli/src/ToDoList.vue
@@ -0,0 +1,53 @@
+<template>
+  <div>
+    <div>
+      <input v-model="inputValue"/>
+      <button @click="submit">提交</button>
+    </div>
+    <div>
+      <ul>
+        <todo-item
+                v-for="(item,index) of list"
+                :key="index"
+                :content="item"
+                :index="index"
+                @delete="handleDelete"
+
+        ></todo-item>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+import ToDoItem from './components/ToDoItem.vue'
+import TodoItem from "./components/ToDoItem";
+export default {
+    components:{
+        TodoItem,
+        'todo-item':ToDoItem
+    },
+  data:function(){
+      return{
+          inputValue:'',
+          list:[]
+      }
+  },
+    methods:{
+      submit:function(){
+          this.list.push(this.inputValue)
+          this.inputValue=''
+      },
+        handleDelete(index){
+          var con = confirm("确定要删除该条记录吗?")
+            if(con){
+              this.list.splice(index,1)
+            }
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
---
+++ ToDoList_Vue-cli/src/main.js
@@ -0,0 +1,8 @@
+import Vue from 'vue'
+import ToDoList from './ToDoList.vue'
+
+
+
+new Vue({
+  render: h => h(ToDoList),
+}).$mount('#app')
